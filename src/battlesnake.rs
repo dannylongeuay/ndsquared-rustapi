71e3cee3b737c78c64c53e2c3fcc7e4e07d326e7
dannylongeuay <dannylongeuay@users.noreply.github.com>
2022-09-30 05:10:09 +0000

    Commit from GitHub Actions (Release and Publish)

--- src/battlesnake.rs
+++ src/battlesnake.rs
@@ -485,7 +485,7 @@ pub fn info() -> Info {
         apiversion: "1".to_owned(),
         author: "DeanRefined".to_owned(),
         customizations,
-        version: "1.6.1".to_owned(),
+        version: "1.7.0".to_owned(),
     }
 }
 
