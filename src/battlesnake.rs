f25aa3297044f2774f4df6a041e066a50a00c3ff
dannylongeuay <dannylongeuay@users.noreply.github.com>
2022-10-07 21:42:00 +0000

    Commit from GitHub Actions (Release and Publish)

--- src/battlesnake.rs
+++ src/battlesnake.rs
@@ -916,7 +916,7 @@ pub fn info() -> Info {
         apiversion: "1".to_owned(),
         author: "DeanRefined".to_owned(),
         customizations,
-        version: "1.9.0".to_owned(),
+        version: "1.10.0".to_owned(),
     };
 
     info!("{:?}", result);
@@ -1058,7 +1058,7 @@ pub mod tests {
         };
         let ruleset = Ruleset {
             name: GameMode::Standard,
-            version: "0.1.0".to_owned(),
+            version: "1.10.0".to_owned(),
             settings,
         };
         let game = Game {
